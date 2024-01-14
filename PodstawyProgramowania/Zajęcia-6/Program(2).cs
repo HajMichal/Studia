5d2b369b7f25992b5ebcc459a3253f4520ad9f48
Michal Haj <92325569+HajMichal@users.noreply.github.com>
2024-01-14 11:04:54 +0100

    Create Program(2).cs

---
+++ PodstawyProgramowania/Zajęcia-6/Program(2).cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.Design.Serialization;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		string x = "Cyryl Kwiecien \n ul.Mokra 4 \n Tychy 40-321";
+
+		Console.WriteLine(x.Replace("\n", ""));
+	}
+}
